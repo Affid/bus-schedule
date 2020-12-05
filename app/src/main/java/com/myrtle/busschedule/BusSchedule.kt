350a6e9c56f79e1ff09653626572ce54a2b2708d
Alena Rebrova <avrebrova@edu.hse.ru>
2020-12-05 17:21:35 +0300

    Фрагмен для автобусов (пока без кнопок)

--- app/src/main/java/com/myrtle/busschedule/BusSchedule.kt
+++ app/src/main/java/com/myrtle/busschedule/BusSchedule.kt
@@ -1,5 +1,37 @@
 package com.myrtle.busschedule
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-class BusSchedule : Fragment()
+class BusSchedule : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(
+            R.layout.bus_fragment,
+            container,
+            false
+        )
+        //TODO Buttons
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val recycler = view.findViewById<RecyclerView>(R.id.bus_recycler)
+        recycler.apply {
+            adapter = BusAdapter(BusRepository.instance.list())
+            layoutManager = LinearLayoutManager(context)
+        }
+    }
+
+}
+
